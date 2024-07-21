7eaf7059cf49a6b465ffa56aa4d853f46a2f5bb2
jumbo <jumbo.creates@gmail.com>
2024-07-21 09:20:39 +0900

    Hello.tsxの削除

---
+++ src/app/event/list.tsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet } from "react-native"
+import Header from "../../components/Header"
+import EventListItem from "../../components/EventListItem"
+import CircleButton from "../../components/CircleButton"
+
+const List = ():JSX.Element =>{
+  return (
+    <View style={styles.container}>
+      {/* ヘッダー */}
+      <Header />
+
+      {/* イベントリスト */}
+      <View>
+        {/* イベント */}
+        <EventListItem />
+        <EventListItem />
+        <EventListItem />
+      </View>
+      {/* イベント追加ボタン */}
+      <CircleButton>
+        +
+      </CircleButton>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  }
+})
+
+export default List
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,7 @@
-import { View, StyleSheet } from "react-native"
-import Header from "../components/Header"
-import EventListItem from "../components/EventListItem"
-import CircleButton from "../components/CircleButton"
+import { Redirect } from "expo-router"
 
-const Index = ():JSX.Element =>{
-  return (
-    <View style={styles.container}>
-      {/* ヘッダー */}
-      <Header />
+const Index = (): JSX.Element => {
+  return <Redirect href="/event/list" />
+ }
 
-      {/* イベントリスト */}
-      <View>
-        {/* イベント */}
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
-      </View>
-      {/* イベント追加ボタン */}
-      <CircleButton>
-        +
-      </CircleButton>
-
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  }
-})
-
-export default Index
+ export default Index
